1b04e8ecf2cbdbbd718e9f31c8c4a70d2502f997
hemillesferreira <hemillesferreira@hotmail.com>
2025-02-04 21:38:12 -0300

    up

---
+++ if_else/script.js
@@ -0,0 +1,6 @@
+function verificar (){
+    let data = new Date ()
+    let ano =  data.getFullYear
+    let nasc = document.getElementById('anoNascimento')
+    var res = document.querySelector ('div#res')
+}
